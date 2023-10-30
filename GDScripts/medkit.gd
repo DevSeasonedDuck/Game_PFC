1e5f0a5a6c6a0573fc22d0f42ead5b117a4823ea
Diego Cárcamo <dcarcamog@fpcoruna.afundacin.org>
2023-10-30 13:16:36 +0100

    Erased unused variable

--- GDScripts/medkit.gd
+++ GDScripts/medkit.gd
@@ -3,7 +3,6 @@ extends Area2D
 @onready var interaction_area=$InteractionArea
 @onready var player=get_tree().get_first_node_in_group("Player")
 @export var cureValue = 5
-var medkit=self
 
 func _ready():
 	interaction_area.interact=Callable(self, "_use_medkit")
